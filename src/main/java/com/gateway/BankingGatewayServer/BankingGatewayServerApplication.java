57717ac60dfa43fdc8fe82510f9a118319b17022
Rayvant Durani <rayvant310@gmail.com>
2024-02-29 11:32:45 +0530

    Adding Gateway to remote Server

---
+++ src/main/java/com/gateway/BankingGatewayServer/BankingGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.BankingGatewayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingGatewayServerApplication.class, args);
+	}
+
+}
